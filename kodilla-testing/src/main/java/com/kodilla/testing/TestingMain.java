70b968ed07dc2c0452255bd468a3cc0c55a7d612
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-05 20:47:49 +0200

    6.2 Added Calculator tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,20 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        if(calculator.add(5,4) == 9) {
+            System.out.println("Test dodawania ok");
+        } else {
+            System.out.println("Błąd testu dodawania");
+        }
+
+        if(calculator.substract(5,3) == 2) {
+            System.out.println("Test odejmowania ok");
+        } else {
+            System.out.println("Błąd testu odejmowania");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int x;
+    private int y;
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int substract(int x, int y) {
+        return x - y;
+    }
+}
