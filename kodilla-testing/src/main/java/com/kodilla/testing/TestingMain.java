6eca27b154e1cdce122e51f62307a835744cdea6
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-06 23:50:10 +0200

    MOD-6.3:ForumTestSuite testy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,20 +1,9 @@
 package com.kodilla.testing;
 import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
 
-        if(calculator.add(5,4) == 9) {
-            System.out.println("Test dodawania ok");
-        } else {
-            System.out.println("Błąd testu dodawania");
-        }
-
-        if(calculator.substract(5,3) == 2) {
-            System.out.println("Test odejmowania ok");
-        } else {
-            System.out.println("Błąd testu odejmowania");
-        }
     }
 }
