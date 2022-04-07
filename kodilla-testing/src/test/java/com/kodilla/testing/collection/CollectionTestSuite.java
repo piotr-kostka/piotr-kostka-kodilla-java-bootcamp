19a540368bc96020b3037a046aac6995625109df
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-07 19:55:26 +0200

    KOD-6.3: zmiany po review - usuniety println, dodany array.aslist, dodane testy sprawdzajace tylko parzyste i tylko nieparzyste przypadki

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -4,6 +4,7 @@ import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CollectionTestSuite {
@@ -37,27 +38,55 @@ public class CollectionTestSuite {
         OddNumbersExterminator exterminator = new OddNumbersExterminator(numbers);
         //When
         boolean result = exterminator.exterminate().isEmpty();
-        System.out.println("Sprawdzanie pustej listy");
         //Then
         Assertions.assertTrue(result);
     }
 
-    @DisplayName("when create OddNumbersExterminator with few numbers in list, " + "then result should return only odd numbers")
+    @DisplayName("when create OddNumbersExterminator with random numbers in list, " + "then result should return only odd numbers")
 
     @Test
     void testOddNumbersExterminatorNormalList() {
+        //Given
         List<Integer> numbers = new ArrayList<>();
         numbers.add(2);
         numbers.add(3);
-
         OddNumbersExterminator exterminator = new OddNumbersExterminator(numbers);
         //When
         List<Integer> result = exterminator.exterminate();
+        List<Integer> expectedResult = Arrays.asList(2);;
+        //Then
+        Assertions.assertEquals(result, expectedResult);
+    }
 
-        List<Integer> expectedResult = new ArrayList<>();
-        expectedResult.add(2);
+    @DisplayName("when create OddNumbersExterminator with odd numbers in list, " + "then result should return all numbers")
 
-        System.out.println("Sprawdzanie parzystych liczb");
+    @Test
+    void testOddNumbersExterminatorOnlyOddList() {
+        //Given
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(6);
+        OddNumbersExterminator exterminator = new OddNumbersExterminator(numbers);
+        //When
+        List<Integer> result = exterminator.exterminate();
+        List<Integer> expectedResult = Arrays.asList(2, 4, 6);;
+        //Then
+        Assertions.assertEquals(result, expectedResult);
+    }
+
+    @DisplayName("when create OddNumbersExterminator with even numbers in list, " + "then result should return null")
+
+    @Test
+    void testOddNumbersExterminatorOnlyEvenList() {
+        //Given
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(3);
+        OddNumbersExterminator exterminator = new OddNumbersExterminator(numbers);
+        //When
+        List<Integer> result = exterminator.exterminate();
+        List<Integer> expectedResult = Arrays.asList();;
         //Then
         Assertions.assertEquals(result, expectedResult);
     }
