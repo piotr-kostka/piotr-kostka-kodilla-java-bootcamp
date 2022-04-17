ff1fcf45af4844101895728a032fdab4b94312aa
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-17 22:41:33 +0200

    KOD-7.5 zmiany po review

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,20 +1,20 @@
 package com.kodilla.stream.array;
 
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
-        double average;
-        if (numbers.length > 0) {
-            IntStream.range(0, numbers.length)
-                    .forEach(number -> System.out.println(numbers[number]));
+        IntStream.range(0, numbers.length)
+            .forEach(number -> System.out.println(numbers[number]));
 
-            average = IntStream.range(0, numbers.length)
-                    .mapToDouble(number -> numbers[number])
-                    .average()
-                    .getAsDouble();
-        } else {return 0;}
+        double average = IntStream.range(0, numbers.length)
+            .filter(Objects::nonNull)
+            .mapToDouble(number -> numbers[number])
+            .average()
+            .orElse(0.00);
+
         return average;
     }
 }
