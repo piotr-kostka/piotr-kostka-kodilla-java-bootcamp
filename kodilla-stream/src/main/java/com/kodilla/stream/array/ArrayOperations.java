5ab3636e77dcd57802fc838dd23373411bc348a2
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-17 17:13:41 +0200

    KOD-7.5: zmiany po review

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -5,14 +5,16 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
-        IntStream.range(0, numbers.length)
-                .forEach(number -> System.out.println(numbers[number]));
+        double average;
+        if (numbers.length > 0) {
+            IntStream.range(0, numbers.length)
+                    .forEach(number -> System.out.println(numbers[number]));
 
-        double average = IntStream.range(0, numbers.length)
-                .mapToDouble(number -> numbers[number])
-                .average()
-                .getAsDouble();
-
+            average = IntStream.range(0, numbers.length)
+                    .mapToDouble(number -> numbers[number])
+                    .average()
+                    .getAsDouble();
+        } else {return 0;}
         return average;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -3,22 +3,29 @@ package com.kodilla.stream.array;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArrayOperationsTestSuite {
 
     @Test
     void testGetAverage() {
         //Given
-        int[] numbers = new int[5];
-        numbers[0] = 5;
-        numbers[1] = 8;
-        numbers[2] = 3;
-        numbers[3] = 6;
-        numbers[4] = 1;
-
+        int[] numbers = {5, 8, 3, 6, 1};
         //When
         double result = ArrayOperations.getAverage(numbers);
         double expected = 4.6;
         //Then
-        Assertions.assertEquals(expected, result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyTable() {
+        //Given
+        int[] numbers = {};
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+        double expected = 0;
+        //Then
+        assertEquals(expected, result);
     }
 }
