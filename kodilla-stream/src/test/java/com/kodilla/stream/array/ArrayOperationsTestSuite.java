035fcc316158fd4b288582283e76fe7a3b1da48b
piotr-kostka <102327925+piotr-kostka@users.noreply.github.com>
2022-04-18 19:24:20 +0200

    KOD-7.5 Zadanie: obliczamy średnią funkcyjnie

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = {5, 8, 3, 6, 1};
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+        double expected = 4.6;
+        //Then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyTable() {
+        //Given
+        int[] numbers = {};
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+        double expected = 0;
+        //Then
+        assertEquals(expected, result);
+    }
+}
