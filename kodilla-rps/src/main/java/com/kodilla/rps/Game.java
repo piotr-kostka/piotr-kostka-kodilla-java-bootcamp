e0856a72f9c08e0cecfd51b6a5d032a0f38a5ee5
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-19 18:17:30 +0200

    KOD-7.7 Rock-Paper-Scissors Game

--- kodilla-rps/src/main/java/com/kodilla/rps/Game.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Game.java
@@ -1,99 +1,167 @@
 package com.kodilla.rps;
 
+import java.util.Random;
+import java.util.Scanner;
 
-import java.util.*;
+import static com.kodilla.rps.Moves.*;
 
-public class Game extends Details {
+public class Game {
+
+    private String playerName;
+    private Moves playerMove;
+    private Moves computerMove;
+    private int pointsToWin;
+    private int playerScore;
+    private int computerScore;
+    boolean end = false;
 
     public Game() {
+        this.playerScore = 0;
+        this.computerScore = 0;
+    }
 
-        Details details = new Details();
-        Scanner input = new Scanner(System.in);
+    private Moves setComputerMove() {
         Random random = new Random();
+        int randomInt = random.nextInt(5);
+        if (randomInt == 0) {
+            return ROCK;
+        } else if (randomInt == 1) {
+            return PAPER;
+        } else if (randomInt == 2) {
+            return SCISSORS;
+        } else if (randomInt == 3) {
+            return LIZARD;
+        } else {
+            return SPOCK;
+        }
+    }
 
-        boolean end = false;
+    private boolean playerWin(Moves playerMove, Moves computerMove) {
+        if (playerMove == ROCK && (computerMove == SCISSORS || computerMove == LIZARD)) {
+            return true;
+        } else if (playerMove == PAPER && (computerMove == ROCK || computerMove == SPOCK)) {
+            return true;
+        } else if (playerMove == SCISSORS && (computerMove == PAPER || computerMove == LIZARD)) {
+            return true;
+        } else if (playerMove == LIZARD && (computerMove == PAPER || computerMove == SPOCK)) {
+            return true;
+        } else if (playerMove == SPOCK && (computerMove == SCISSORS || computerMove == ROCK)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        HashMap<String, Move> playerChoiceMap = new HashMap<>();
-        playerChoiceMap.put("1", new Kamien());
-        playerChoiceMap.put("2", new Papier());
-        playerChoiceMap.put("3", new Nozyce());
+    private boolean computerWin(Moves playerMove, Moves computerMove) {
+        if (computerMove == ROCK && (playerMove == SCISSORS || playerMove == LIZARD)) {
+            return true;
+        } else if (computerMove == PAPER && (playerMove == ROCK || playerMove == SPOCK)) {
+            return true;
+        } else if (computerMove == SCISSORS && (playerMove == PAPER || playerMove == LIZARD)) {
+            return true;
+        } else if (computerMove == LIZARD && (playerMove == PAPER || playerMove == SPOCK)) {
+            return true;
+        } else if (computerMove == SPOCK && (playerMove == SCISSORS || playerMove == ROCK)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        List<Move> computerChoiceList = new ArrayList<>();
-        computerChoiceList.add(new Kamien());
-        computerChoiceList.add(new Papier());
-        computerChoiceList.add(new Nozyce());
+    private boolean draw(Moves playerMove, Moves computerMove) {
+        if (playerMove == ROCK && computerMove == ROCK) {
+            return true;
+        } else if (playerMove == PAPER && computerMove == PAPER) {
+            return true;
+        } else if (playerMove == SCISSORS && computerMove == SCISSORS) {
+            return true;
+        } else if (playerMove == LIZARD && computerMove == LIZARD) {
+            return true;
+        } else if (playerMove == SPOCK && computerMove == SPOCK) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        details.getStartTitle();
-        details.setPlayerName();
-        details.setRoundsToWin();
-
-        while (!end) {
-
-            System.out.println("Wybierz: 1 - Kamień, 2 - Papier, 3 - Nożyce" + "\n" + "X - Zamknij grę, N - Nowa gra");
-            String playerChoice = input.nextLine();
-            Move playerMove = playerChoiceMap.get(playerChoice);
-
-            if (playerChoice.equals("X")) {
-                System.out.println("Czy chcesz zakończyć grę? T - tak, N - nie");
+    public void play() {
+        Scanner input = new Scanner(System.in);
+        Dialogs.startMenu();
+        playerName = Dialogs.setPlayerName();
+        pointsToWin = Dialogs.setRoundsToWin();
+        while(!end) {
+            playerMove = Dialogs.setPlayerMove();
+            if (playerMove == NEW_GAME) {
+                System.out.println("Starting new game!");
+                playerScore = 0;
+                computerScore = 0;
+                play();
+            }
+            if (playerMove == END_GAME) {
+                System.out.println("Are you sure? Y - yes, N - no");
                 String choice = input.nextLine().toUpperCase();
-
-                if (choice.equals("T")) {
-                    System.out.println("Koniec gry. Aplikacja zamknięta");
+                if (choice.equals("Y")) {
+                    System.out.println("Ending game. Application will be closed!");
+                    end = true;
+                } else if (choice.equals("N")) {
+                    continue;
+                } else {
+                    System.out.println("Choose Y/N");
+                    continue;
+                }
+            }
+            if (playerMove == ROCK || playerMove == PAPER || playerMove == SCISSORS || playerMove == LIZARD || playerMove == SPOCK) {
+                computerMove = setComputerMove();
+                if (playerWin(playerMove, computerMove)) {
+                    System.out.println("Player choose: " + playerMove + " VS " + computerMove + "   " + playerName + " won!");
+                    playerScore++;
+                    System.out.println("Current score: " + playerName + " " + playerScore + ":" + computerScore + " COMPUTER");
+                } else if (computerWin(playerMove, computerMove)) {
+                    System.out.println("Player choose: " + playerMove + " VS " + computerMove + "   " + "COMPUTER won!");
+                    computerScore++;
+                    System.out.println("Current score: " + playerName + " " + playerScore + ":" + computerScore + " COMPUTER");
+                } else if (draw(playerMove, computerMove)) {
+                    System.out.println("Player choose: " + playerMove + " VS " + computerMove);
+                    System.out.println("DRAW! No points!");
+                    System.out.println("Current score: " + playerName + " " + playerScore + ":" + computerScore + " COMPUTER");
+                }
+            }
+            if (playerScore == pointsToWin) {
+                System.out.println("CONGRATULATIONS! YOU WON!");
+                System.out.println("FINAL SCORE: " + playerName + " " + playerScore + ":" + computerScore + " COMPUTER");
+                System.out.println("Play another game?  Y - YES   N - NO");
+                String choice = input.nextLine().toUpperCase();
+                if (choice.equals("Y")) {
+                    System.out.println("Starting new game!");
+                    playerScore = 0;
+                    computerScore = 0;
+                    play();
+                } else if (choice.equals("N")) {
+                    System.out.println("Ending game. Application will be closed!");
                     end = true;
                 } else {
+                    System.out.println("Choose Y/N");
                     continue;
                 }
-            } else if (playerChoice.equals("N")) {
-                System.out.println("Czy chcesz rozpocząć grę od nowa? T - tak, N - nie");
+            } else if (computerScore == pointsToWin) {
+                System.out.println("GAME OVER! COMPUTER WON!");
+                System.out.println("FINAL SCORE: " + playerName + " " + playerScore + ":" + computerScore + " COMPUTER");
+                System.out.println("Play another game?  Y - YES   N - NO");
                 String choice = input.nextLine().toUpperCase();
-
-                if (choice.equals("T")) {
-                    details.newGame();
+                if (choice.equals("Y")) {
+                    System.out.println("Starting new game!");
+                    playerScore = 0;
+                    computerScore = 0;
+                    play();
+                } else if (choice.equals("N")) {
+                    System.out.println("Ending game. Application will be closed!");
+                    end = true;
                 } else {
+                    System.out.println("Choose Y/N");
                     continue;
                 }
-            } else if (playerChoice.equals("1") || playerChoice.equals("2") || playerChoice.equals("3")) {
-                System.out.println("Wybrałeś: " + playerMove.name());
-
-                int computerChoice = random.nextInt(3);
-                Move computerMove = computerChoiceList.get(computerChoice);
-                System.out.println("Komputer wybrał: " + computerMove.name());
-
-                RoundResult result = playerMove.winsWithMove(computerMove);
-
-                if (result.isDraw()) {
-                    details.draw();
-                } else if (result.isWin()) {
-                    details.playerWin();
-                    if (details.playerScore == details.roundsToWin && details.playerScore > details.computerScore) {
-                        details.playerWonGame();
-                        System.out.println("Czy chcesz zagrać jeszcze raz? T - tak, N - nie");
-                        String choice = input.nextLine().toUpperCase();
-
-                        if (choice.equals("T")) {
-                            details.newGame();
-                        } else {
-                            end = true;
-                        }
-                    } else {
-                        continue;
-                    }
-                } else {
-                    details.computerWin();
-                    if (details.computerScore == details.roundsToWin && details.computerScore > details.playerScore) {
-                        details.computerWonGame();
-                        System.out.println("Czy chcesz zagrać jeszcze raz? T - tak, N - nie");
-                        String choice = input.nextLine().toUpperCase();
-
-                        if (choice.equals("T")) {
-                            details.newGame();
-                        } else {
-                            end = true;
-                        }
-                    } else {
-                        continue;
-                    }
-                }
+            } else {
+                continue;
             }
         }
     }
--- kodilla-rps/src/main/java/com/kodilla/rps/Move.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.rps;
-
-public interface Move {
-
-    public String name();
-    public RoundResult winsWithMove(Move computer);
-}
