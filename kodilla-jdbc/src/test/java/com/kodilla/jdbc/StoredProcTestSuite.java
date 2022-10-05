acd3bd45874eaf311b664260114a9912741e29de
piotr-kostka <102327925+piotr-kostka@users.noreply.github.com>
2022-10-05 15:39:18 +0200

    KOD-30.3 Zadanie: wywołanie procedury UpdateBestsellers z poziomu Javy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -33,4 +33,29 @@ public class StoredProcTestSuite {
         rs.close();
         statement.close();
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = 0";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = 1";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        //Then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(1, howMany);
+        rs.close();
+        statement.close();
+    }
 }
